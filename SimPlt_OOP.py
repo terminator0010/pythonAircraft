65b15d2e282abe5eaadbe17d4d696e17da9a1e9f
terminator0010 <34940921+terminator0010@users.noreply.github.com>
2025-04-09 22:52:03 -0300

    Add files via upload

--- SimPlt_OOP.py
+++ SimPlt_OOP.py
@@ -26,51 +26,66 @@ class FlightSimulation():
         self.u = 100
         self.v = 0
         self.w = 0
-        self.m = 0 #Massa kg
-        self.ib = 0
-        self.jb = 0
-        self.kb = 0
-        self.theta = 5
+        self.m = 1000 #Massa kg
+        self.g = 9.81
+        self.ib = 1
+        self.jb = 1
+        self.kb = 1
+        self.theta = 0
         self.phi = 0
         self.psi = 0
         self.p = 0
         self.q = 0
         self.r = 0
-        self.xlim, self.ylim, self.zlim = 1300, 1300, 300  # Ajuste conforme necessário
-        self.x_advance = 0
-        self.y_advance = 0
-        self.z_advance = 0
+        self.xlim, self.ylim, self.zlim = 1300, 1300, 300  # Ajuste conforme necessário   
         self.x_values = []
         self.y_values = []
         self.z_values = []
         
-     
+    
+    def atmospheric(self):
+        self.rho = 1.225
+        self.P0_PA = 101325
+        self.k = -0.0065
+        self.Gamma = 1.4
+        self.T0 = 288.15
+        self.R = 287    
+        
+        
+        self.Temp_k = -self.z*self.k + self.T0
+        self.Pressura_PA = (self.Temp_k * (1/self.T0)+math.pow(-self.g/(-self.k*self.Gamma))*self.P0_PA)*(math.exp((-self.g/self.R)/self.Temp_k)*(self.z-11000))
+        self.SpeedofSound_mps = math.sqrt(self.Temp_k*self.R*self.Gamma)
+        self.Rho_kgpm3 = self.Pressura_PA/self.Temp_k * self.R
+        
+        
 
     def flightMechannics(self):
         # Criar a figura e o eixo 3D antes do loop           
         for t in range(int(maxtime/dt) +1):
             
+            weight_N = self.m * self.g
+            
             theta = d2r(-self.theta) 
             phi = d2r(self.phi)
             psi = d2r(self.psi)
             gamma = d2r(math.atan2(self.w, self.u))
-
+            
             i2 = self.ib
             k1 = -math.cos(theta)*self.ib + math.cos(theta)*math.sin(phi)*self.jb + math.cos(phi) * math.cos(theta)*self.kb
             j2 = math.cos(phi)*self.jb - math.sin(phi)*self.kb
 
-            p = (phi_dot - psi_dot*math.sin(theta))
-            q = (theta_dot*math.cos(phi )+ phi_dot*math.cos(phi)*math.sin(phi))
-            r = (psi_dot*math.cos(phi)*math.cos(theta)- theta_dot*math.sin(phi))
+            self.p = (phi - self.psi*math.sin(theta))
+            self.q = (theta*math.cos(phi )+ phi*math.cos(phi)*math.sin(phi))
+            self.r = (psi*math.cos(phi)*math.cos(theta)- theta*math.sin(phi))
 
-            phi_dot = p + q*math.sin(phi)*math.tan(theta) + r*math.cos(phi)*math.tan(theta)
-            theta_dot = q*math.cos(phi) - r*math.sin(phi)
-            psi_dot = (q*math.sin(phi) + r*math.cos(phi))/math.cos(theta)
-
+            phi = self.p + self.q*math.sin(phi)*math.tan(theta) + self.r*math.cos(phi)*math.tan(theta)
+            theta = self.q*math.cos(phi) - self.r*math.sin(phi)
+            psi = (self.q*math.sin(phi) + self.r*math.cos(phi))/math.cos(theta)
+            '''
             u_dot = r*self.v - q*self.w+ TransMotion(1)/self.m
             v_dot = p*self.p - r*self.u + TransMotion(2)/self.m
             w_dot = q*self.u - p*self.v + TransMotion(3)/self.m
-
+            
             X_dot = [u_dot, v_dot, w_dot]
             X_matrix = [self.u, self.v, self.w]
             #omega = self.ib*p + self.jb*q + self.kb*r
@@ -83,11 +98,12 @@ class FlightSimulation():
             alpha = theta - gamma
             V = math.sqrt(pow(self.u)+pow(self.v)+pow(self.w,))
             beta = math.asin(self.v,V)
+            '''
 
             #x = self.x + dt*(self.u*math.cos(psi)*math.cos(theta) - self.v*math.sin(psi) + self.w* math.cos(theta))
-            self.x = self.x + dt * (self.u * math.cos(psi) * math.cos(theta) + self.v * (math.sin(phi) * math.sin(psi) * math.cos(psi)) + self.w * (math.cos(phi) * math.sin(theta) * math.cos(psi) + math.sin(phi)*math.sin(psi)))# + dt * self.x_advance
-            self.y = self.y + dt * (self.u * math.sin(psi) * math.cos(theta) + self.v * (math.cos(psi) * math.sin(theta) * math.sin(phi)) + self.w * (math.cos(phi) * math.sin(theta) * math.sin(psi) - math.sin(phi)* math.cos(psi))) #+ dt * self.y_advance
-            self.z = self.z + dt * (-self.u * math.sin(theta) + self.v * math.cos(theta)* math.sin(phi) * self.w * math.cos(theta)* math.cos(phi)) # dt * self.z_advance
+            self.x = (self.x + dt * (self.u * math.cos(psi) * math.cos(theta) + self.v * (math.sin(phi) * math.sin(psi) * math.cos(psi)) + self.w * (math.cos(phi) * math.sin(theta) * math.cos(psi) + math.sin(phi)*math.sin(psi))))
+            self.y = (self.y + dt * (self.u * math.sin(psi) * math.cos(theta) + self.v * (math.cos(psi) * math.sin(theta) * math.sin(phi)) + self.w * (math.cos(phi) * math.sin(theta) * math.sin(psi) - math.sin(phi)* math.cos(psi))))
+            self.z = (self.z + dt * (-self.u * math.sin(theta) + self.v * math.cos(theta)* math.sin(phi) * self.w * math.cos(theta)* math.cos(phi)))
             
             
                
@@ -114,6 +130,7 @@ class FlightSimulation():
         ax.set_ylabel('X ' + 'Roll')
         ax.set_zlabel('Z ' + 'Yaw')
         ax.legend()
+        
 
         plt.pause(0.01)  # Pequena pausa para atualizar a animação
         plt.show()
